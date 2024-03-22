d8bd0c7a7ad0790bcd8cee02468533dc5dbac4ca
ThiagoComba <thiagocomba@hotmail.com>
2024-03-22 13:50:44 -0300

    Final commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplicacao (PrimeiroNumero: number, SegundoNumero: number): number {
+    return PrimeiroNumero * SegundoNumero 
+}
+
+function dizOla (nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(multiplicacao(8, 4))
+console.log(dizOla('Thiago'))
